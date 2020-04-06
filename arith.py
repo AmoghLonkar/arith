3fe618d367d0da14f53468e16c3aadba13780330
AmoghLonkar <alonkar@trashpanda.soe.ucsc.edu>
2020-04-05 17:55:14 -0700

    Bare bones token class implemented

---
+++ arith.py
@@ -0,0 +1,32 @@
+#Referenced 'https://ruslanspivak.com/lsbasi-part1/'
+#Referenced 'https://ruslanspivak.com/lsbasi-part7/'
+
+#Token Class
+#Token has type and value
+#Token types: integer, add, sub, mul
+class Token:
+
+    #Token "constructor"
+    def __init__(self, type,value):
+        self.type = type
+        self.value = value
+
+#Lexer Class
+#Lexer reads input string and splits into tokens
+class Lexer:
+
+    #Lexer "constructor"
+    def __init(self, string):
+       print('') 
+
+def main():
+    while True:
+        expression = raw_input("Enter expression: ")
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
